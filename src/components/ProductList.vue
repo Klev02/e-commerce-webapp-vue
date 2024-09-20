7e1b5bf90aa6e02f3b04ee5ab41d589f9526f94b
Klev02 <kollarlevi@gmail.com>
2024-09-20 16:24:23 +0200

    fetch data and print raw response

---
+++ src/components/ProductList.vue
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<template>
+    <p>List</p>
+</template>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,6 +1,43 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import type { Product } from '../interfaces/Product';
+
+const API_URL = "https://63c10327716562671870f959.mockapi.io/products";
+
+const products = ref<Product[]>([]);
+const isLoading = ref<boolean>(false);
+const hasError = ref<boolean>(false);
+
+async function fetchProducts() {
+  isLoading.value = true;
+  hasError.value = false;
+
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error('SOmething went wrong!');
+    }
+
+    products.value = await response.json();
+  } catch (error) {
+    console.log(error);
+    hasError.value = true;
+  } finally {
+    isLoading.value = false;
+  }
+}
+
+onMounted(() => {
+  fetchProducts();
+})
+
 </script>
 
 <template>
   <p>Products</p>
+  <p v-if="isLoading">Loading...</p>
+  <p v-else-if="hasError">Something went wrong</p>
+  <p v-else-if="!isLoading && !hasError && products.length > 0">
+    {{ JSON.stringify(products) }}
+  </p>
 </template>
